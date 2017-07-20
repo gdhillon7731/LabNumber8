b93db01b39631ea8701e4f4c1e0cfe4c0f372268
gdhillon7731 <dhillon7731@gmail.com>
2017-07-20 15:44:09 -0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         //welcome prompt
-        System.out.println("Welcome to the Batting Average Calculator!");
+        System.out.println("\nWelcome to the Batting Average Calculator!");
         //Asking user input for number of times ar bat
         String answer = "y";
+        int bases = 0;
+        double earnedBase = 0.000;
 
         do {
             System.out.print("\nEnter number of times at bat:\n");
@@ -23,23 +26,22 @@ public class Main {
 
 
             for (i = 0; i < array.length; i++) {
-                System.out.print("Result for at bat " + i + ":");
+                System.out.print("Result for at bat " + (i + 1) + ":");
                 int basesEarned = input.nextInt();
-
+                input.nextLine();
 
                 array[i] = basesEarned;
                 sum = sum + array[i];
-                batting = basesEarned;
 
-                if (i > 0 && i <= 4) {
-                    battingAve = array[i];
 
+                bases += array[i];
+                if (array[i] > 0) {
+                    earnedBase++;
                 }
-
 
             }
 
-            System.out.println("Batting average: " + (num / battingAve));
+            System.out.println("Batting average: " + (earnedBase / num));
             System.out.println("Slugging Percentage: " + (sum / num));
 
             System.out.println("Another batter? (y/n)");
@@ -51,14 +53,3 @@ public class Main {
     }
 }
 
-
-//        Scanner input = new Scanner(System.in);
-//        //Read user  input.
-//        System.out.println("How many numbers do you want to enter?");
-//        int num = input.nextInt();
-//        int array[] = new int[num];
-//        System.out.println("Enter the " + num + " numbers now.");
-//        for (int i = 0 ; i < array.length; i++ )
-//        {
-//        array[i] = input.nextInt();
-//        }
